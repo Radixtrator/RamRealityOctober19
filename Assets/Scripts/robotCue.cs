47647215dbcf58ac32959b8120c87daaace57819
Lucas <lucas.plabst@uni-wuerzburg.de>
2019-10-20 11:11:41 -0600

    Talking Store

--- Assets/Scripts/robotCue.cs
+++ Assets/Scripts/robotCue.cs
@@ -6,25 +6,32 @@ public class robotCue : MonoBehaviour
 {
     public AudioClip robot_talking;
     public AudioSource robot;
-    public Vector3 past;
-
+    public GameObject Banana;
+    public Vector3 BananaPast;
+    public GameObject Apple;
+    public Vector3 ApplePast;
+    public GameObject Orange;
+    public Vector3 OrangePast;
     // Start is called before the first frame update
     void Start()
     {
-        past = gameObject.transform.position;
+        ApplePast = Apple.transform.position;
+        OrangePast = Orange.transform.position;
+        BananaPast = Banana.transform.position;
         robot.clip = robot_talking;
-
     }
 
     // Update is called once per frame
     void Update()
     {
-            if (Vector3.Distance(past, gameObject.transform.position) > .1)
+            if ((Vector3.Distance(ApplePast, Apple.transform.position) > .2) || (Vector3.Distance(OrangePast, Orange.transform.position) > .2) || (Vector3.Distance(BananaPast, Banana.transform.position) > .2))
             {
                 Debug.Log("here");
                 robot.Play();
             }
-            past = gameObject.transform.position;
-     }
+            ApplePast = Apple.transform.position;
+            BananaPast = Banana.transform.position;
+            OrangePast = Orange.transform.position;
+    }
 
     }
