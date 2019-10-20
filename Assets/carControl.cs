6ca4bac3902fe720d22e2314ef0653f8770dd21c
Faezeh <faezeborhani@gmail.com>
2019-10-20 11:16:00 -0600

    Added random cars and some people!

---
+++ Assets/carControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carControl : MonoBehaviour
+{
+    public List<GameObject> cars = new List<GameObject>();
+    //Random random = new Random();
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        foreach (var car in cars)
+        {
+            car.SetActive(false);
+        }
+    }
+    void Start()
+    {
+        StartCoroutine(carGenerator());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    IEnumerator carGenerator()
+    {
+        var random = new Random();
+        while (true)
+        {
+            int index = Random.Range(0, cars.Count);
+            GameObject car = cars[index];
+            if(car.transform.position.z < -100)
+            {
+                cars[index].SetActive(false);
+                cars[index].SetActive(true);
+            } 
+            
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
