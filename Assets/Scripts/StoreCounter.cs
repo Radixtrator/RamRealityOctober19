d0b0107805b54952296803d1f958aea5839e6434
Lucas <lucas.plabst@uni-wuerzburg.de>
2019-10-20 11:51:57 -0600

    Store Audio

--- Assets/Scripts/StoreCounter.cs
+++ Assets/Scripts/StoreCounter.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class StoreCounter : MonoBehaviour
 {
     public int items = 0;
+    public GameObject AudioManager;
+    private StoreAudio Sa;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Sa = AudioManager.GetComponent<StoreAudio>();
     }
 
     // Update is called once per frame
@@ -25,6 +27,18 @@ public class StoreCounter : MonoBehaviour
             Destroy(collision.gameObject);
             Debug.Log(collision);
         }
-      
+        if(collision.gameObject.layer == 8)
+        {
+            Sa.PlayApple();
+        }
+        if (collision.gameObject.layer == 9)
+        {
+            Sa.PlayOrange();
+        }
+        if (collision.gameObject.layer == 10)
+        {
+            Sa.PlayBanana();
+        }
+
     }
 }
---
+++ Assets/StoreAudio.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreAudio : MonoBehaviour
+{
+    public bool WheelChair;
+    public GameObject Apple;
+    public GameObject Banana;
+    public GameObject Orange;
+    public AudioClip BananaClip;
+    public AudioClip OrangeClip;
+    public AudioClip AppleClip;
+    public AudioClip GreatingClip;
+    public AudioSource Robot;
+    void Start()
+    {
+        if(!WheelChair)
+        {
+            Debug.Log("here");
+            Robot.clip = GreatingClip;
+            Robot.Play();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (WheelChair && Input.GetKeyDown("space"))
+        {
+            Robot.clip = GreatingClip;
+            Robot.Play();
+        }
+    }
+    public void PlayApple()
+    {
+        Robot.clip = AppleClip;
+        Robot.Play();
+    }
+    public void PlayOrange()
+    {
+        Robot.clip = OrangeClip;
+        Robot.Play();
+    }
+    public void PlayBanana()
+    {
+        Robot.clip = BananaClip;
+        Robot.Play();
+    }
+}
